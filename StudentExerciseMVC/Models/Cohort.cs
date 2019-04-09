1f6d72604be29987b172ccbb7f03117de100bac9
Colleen Woolsey <rlwool@aol.com>
2019-04-08 21:32:28 -0500

    Models and Student Controller

---
+++ StudentExerciseMVC/Models/Cohort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExerciseMVC.Models
+{
+    public class Cohort
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(11, MinimumLength = 5)]
+        // Message "Cohort name should be in the format of [Day|Evening] [number]"
+        [RegularExpression(@"(\bday\b|\bDay\b|\bevening\b|\bEvening\b)\s(\b\d{1,2})")]
+        public string CohortName { get; set; }
+
+        public List<Student> ListofStudents { get; set; } = new List<Student>();
+
+        public List<Instructor> ListofInstructors { get; set; } = new List<Instructor>();
+    }
+}
---
+++ StudentExerciseMVC/Models/Instructor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExerciseMVC.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        [Required]
+        public string InstructorFirstName { get; set; }
+
+        [Required]
+        public string InstructorLastName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string InstructorSlackHandle { get; set; }
+
+        public string CohortName { get; set; }
+
+        [Required]
+        public int CohortId { get; set; }
+
+        public Cohort Cohort { get; set; }
+    }
+}
